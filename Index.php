0a3d9558475d96b214c4cf309372525375993d6d
YassinDickel <92588449+YassinDickel@users.noreply.github.com>
2024-04-16 10:03:52 +0200

    index en stylesheet

--- Index.php
+++ Index.php
@@ -1,1 +1,23 @@
-<?php
+<!doctype html>
+<html>
+<head>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<div class="sidebar">
+
+    <div class="technologo">
+        <a href="index.php"><img src="Images/technolab.png" alt="TechnoLab"></a>
+    </div>
+    <ul>
+        <li><a href="Land">Landen</a></li>
+        <li><a href="Ingrediënt">Ingrediënten</a></li>
+        <li><a href="Persoon">Personen</a></li>
+        <li><a href="Resultaat">Resultaten</a></li>
+    </ul>
+</div>
+<div class="content">
+
+</div>
+</body>
+</html>
